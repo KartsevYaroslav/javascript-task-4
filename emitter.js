0725f15cf9db2467f2b8fd1185abdb51bf9db340
kartsev <kartsev@skbkontur.ru>
2018-11-04 17:59:04 +0500

    try resolve common task

--- emitter.js
+++ emitter.js
@@ -4,40 +4,79 @@
  * Сделано задание на звездочку
  * Реализованы методы several и through
  */
-const isStar = true;
+const isStar = false;
 
 /**
  * Возвращает новый emitter
  * @returns {Object}
  */
+
 function getEmitter() {
     return {
 
+        events: {},
+
         /**
+         * @returns {Object}
          * Подписаться на событие
          * @param {String} event
          * @param {Object} context
          * @param {Function} handler
          */
         on: function (event, context, handler) {
+            const eventsNames = event.split('.');
             console.info(event, context, handler);
+            let con = this.getNestedEvent(eventsNames, eventsNames.length);
+            con.observes.push({ context: context, handler: handler });
+
+            return this;
         },
 
         /**
+         * @returns {Object}
          * Отписаться от события
          * @param {String} event
          * @param {Object} context
          */
         off: function (event, context) {
+            const eventsNames = event.split('.');
+            const nestedEvent = this.getNestedEvent(eventsNames, eventsNames.length);
+            const index = nestedEvent.observes.findIndex(x => x.context === context);
+            nestedEvent.observes.splice(index, 1);
             console.info(event, context);
+
+            return this;
+        },
+
+        getNestedEvent: function (eventsNames, deepLvl) {
+            let context = this.events;
+            for (let i = 0; i < deepLvl; i++) {
+                if (context[eventsNames[i]] === undefined) {
+                    context[eventsNames[i]] = {
+                        observes: []
+                    };
+                }
+                context = context[eventsNames[i]];
+            }
+
+            return context;
         },
 
         /**
+         * @returns {Object}
          * Уведомить о событии
          * @param {String} event
          */
         emit: function (event) {
+            const eventsNames = event.split('.');
+            let con;
+            for (let i = 0; i < eventsNames.length; i++) {
+                con = this.getNestedEvent(eventsNames, eventsNames.length - i);
+                con.observes.forEach(x => x.handler.call(x.context));
+            }
             console.info(event);
+
+            return this;
         },
 
         /**
